a8f155057a901a741b923ce32c8ab3471eb65fb7
Javier Ballón <javi4195@gmail.com>
2025-01-08 16:58:25 -0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './modules/dashboard/pages/dashboard.component';
+import { HomeComponent } from './modules/dashboard/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Redirección al dashboard
+    { path: 'dashboard', component: DashboardComponent }, // Ruta para el dashboard
+    { path: 'home', component: HomeComponent }, // Ruta para home
+];
